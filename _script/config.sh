5e93e77f74a0381f6e46aa8c47604ab7ff231c7a
EnigmaCurry <ryan@enigmacurry.com>
2025-06-11 12:37:37 -0600

    qos

--- _script/config.sh
+++ _script/config.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
-
-set -e
+check_var SCRIPT_DIR
+source ${SCRIPT_DIR}/ssid.sh
 
 FEDORA_DEPENDENCIES=(
     podman ax25-apps ax25-tools jq git curl expect bluez bluez-tools bluez-deprecated kernel-modules-extra
@@ -11,7 +10,7 @@ DEBIAN_DEPENDENCIES=(
 )
 
 dependencies() {
-    check_root || fault "You must run this script as root."
+    check_not_root || fault "You must not run this script as root (but you do need sudo privileges)."
     check_is_systemd || fault "Sorry, only machines with systemd are supported."
     if check_is_debian; then
         install_packages ${DEBIAN_DEPENDENCIES[@]}
@@ -23,9 +22,7 @@ dependencies() {
         exit 1
     fi
     install_script_wizard ${QOS_DIR}/_script
-
 }
-
 
 pair() {
     RFCOMM_MAC_ADDRESS=$(get RFCOMM_MAC_ADDRESS)
@@ -118,9 +115,34 @@ configure_ax25_ports() {
         | sudo tee /etc/ax25/axports
 }
 
+validate_base_callsign() {
+    local cs="$*"
+    if [[ "$cs" =~ ^[A-Za-z0-9]{1,7}$ ]]; then
+        return 0
+    else
+        stderr "Invalid base callsign. Must be 1–7 letters or digits (e.g. N0CALL)."
+        return 1
+    fi
+}
+
+validate_station_callsign() {
+    local scs="$*"
+    # base is 1–7 alnum, SSID 0–15
+    if [[ "$scs" =~ ^[A-Za-z0-9]{1,7}-([0-9]|1[0-5])$ ]]; then
+        return 0
+    else
+        stderr "Invalid station callsign. Must be \`CALLSIGN-SSID\` where SSID is 0–15 (e.g. N0CALL-3)."
+        return 1
+    fi
+}
+
 configure_callsign() {
-    ask_valid CALLSIGN "Enter your callsign:" upcase validate_callsign
+    ask_valid CALLSIGN "Enter your callsign (No station id suffix):" upcase validate_base_callsign
     save CALLSIGN
+}
+
+configure_ssid() {
+    echo
 }
 
 configure_rfcomm_service() {
@@ -192,20 +214,6 @@ EOF
     sudo systemctl enable --now ax25d
 }
 
-config() {
+settings() {
     configure_callsign
-    configure_ax25_ports
-    # echo
-    # configure_sound_device
-    # echo
-    # configure_volume_input_device
-    # echo
-    # configure_volume_output_device
-    # echo
-    # configure_ptt_rts
-    echo
-    echo "########################################"
-    echo "## Configuration complete."
-    echo "########################################"
-    echo
 }
